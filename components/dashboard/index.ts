2ce8e1ddaca8182f1a62b32481239682723fe5b3
brokolink13-crypto <brokolink13@gmail.com>
2026-09-13 12:45:50 +0700

    refactor: split dashboard into smaller components for maintainability

--- components/dashboard/index.ts
+++ components/dashboard/index.ts
@@ -2,3 +2,4 @@ export { BrokoAvatar } from "./BrokoAvatar";
 export { BrokoHeroCharacter } from "./BrokoHeroCharacter";
 export { BrokoWandCharacter } from "./BrokoWandCharacter";
 export { BrokoInsightThumb } from "./BrokoInsightThumb";
+export { CTABannerCard, RecentVideoSection, QuickActionsGrid, BrokoInsightCard } from "./DashboardSections";
